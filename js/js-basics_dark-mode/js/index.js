3820abd51755cf2a3181e18f11a97e7b83bec2f0
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-03-17 16:43:13 +0100

    Dark mode done

--- js/js-basics_dark-mode/js/index.js
+++ js/js-basics_dark-mode/js/index.js
@@ -1,3 +1,19 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkbutton = document.querySelector('[data-js="dark-mode-button"]');
+darkbutton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+const lightbutton = document.querySelector('[data-js="light-mode-button"]');
+lightbutton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  // We can't do add("light") cause we dont have body.light in css//
+});
+
+const togglebutton = document.querySelector('[data-js="toggle-button"]');
+togglebutton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
