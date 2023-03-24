eede4d565b842b430e52c393d5d6ab3623a2c0eb
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-03-24 15:39:55 +0100

    added challenges

--- js/js-createelement_product-teaser/js/index.js
+++ js/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,41 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+// Below here is not my code This is Aps's
+const secondArticle = document.createElement("article");
+secondArticle.classList.add("product");
+secondArticle.innerHTML = `
+      <div class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+            ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src=${imageSrc}
+            alt=""
+          />
+        </div>
+      </div>
+      <footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button">Buy</button>
+      </footer>
+`;
+document.body.append(secondArticle);
+const secondButton = document.querySelector(
+  "body > article:nth-child(3) > footer > button"
+);
+
+secondButton.addEventListener("click", () => {
+  console.log(name, price);
+});
