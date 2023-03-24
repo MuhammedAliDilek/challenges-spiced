48cb04c75a6c1bc855db52436c08e612db6149c1
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-03-24 11:30:10 +0100

    toast done

--- js/js-createelement_toast-messages/js/index.js
+++ js/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,15 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const listElement = document.createElement("li");
+  console.log(toastContainer);
+  listElement.classList.add("toast-container__message");
+  listElement.textContent = "I am a burned toast!";
+  toastContainer.append(listElement);
+  //toastContainer.innerHTML =add "burned toast";
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
