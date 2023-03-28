dbb1ff09fd52acac61fb6d91a0774d9ca03edbe5
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-03-28 17:44:06 +0200

    update

--- js/js-loops_while-random-number/index.js
+++ js/js-loops_while-random-number/index.js
@@ -6,6 +6,10 @@ let number = 0;
 let count = 0;
 
 // --v-- write/change code here --v--
+while (string.length <= 8) {
+  console.log(string);
+  string = string + string;
+}
 
 // --^-- write/change code here --^--
 
