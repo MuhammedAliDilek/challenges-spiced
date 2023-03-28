8ffe22985cb776bbeea57381bdf3f83fb30bf6b1
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-03-28 14:30:43 +0200

    for loop done

--- js/js-loops_for-console/index.js
+++ js/js-loops_for-console/index.js
@@ -1,1 +1,26 @@
 console.clear();
+
+/* ## Task
+
+Log the numbers from 0 to 100 (including) into the console. Use a for loop.
+
+Make sure your for loop has
+
+- an internal counter variable
+- a criterion to be met by the counter variable
+- a rule how the counter variable is increased */
+
+/* let string = "0";
+
+while (string.length <= 100) {
+  console.log(string);
+  string = string + string;
+} */
+
+let i = 0;
+
+// While loop from 0 to 100
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
