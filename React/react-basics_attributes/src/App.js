b57b09ef32a109eed8e6253f264afe3b46a2b340
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-04-11 16:02:29 +0200

    attributes done

--- React/react-basics_attributes/src/App.js
+++ React/react-basics_attributes/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article>Replace me with an article</Article>;
+}
+function Article() {
+  return (
+    <article>
+      <h2 className="article_title">I am the title</h2>
+      <label htmlFor="my-input">I am the label</label>
+      <input type="text" id="my-input" />
+    </article>
+  );
 }
