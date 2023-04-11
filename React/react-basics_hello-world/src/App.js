88eda87d3e53ea8309061e5473a3199a9e5b9c36
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-04-11 15:16:31 +0200

    Hello world done

--- React/react-basics_hello-world/src/App.js
+++ React/react-basics_hello-world/src/App.js
@@ -2,5 +2,7 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return <HelloWorld>Say hello world...</HelloWorld>;
 }
+
+function HelloWorld()
