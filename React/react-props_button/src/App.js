c7efc106f49e6a1d3533843488254873058feaed
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-04-12 16:24:40 +0200

    button done

--- React/react-props_button/src/App.js
+++ React/react-props_button/src/App.js
@@ -2,5 +2,35 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+function handleClick(dateoftoday) {
+  alert("Today is ${dateoftoday}!");
+}
+return(
+<Button
+buttonColor="#ffffff"
+buttonBackgroundColor="#fc1303"
+disabled={false}
+text="Date of Today !"
+onPressButton={() => handleClick(new Date().toLocaleDateString("en-US"))}
+/>
+);
+}
+function Button({
+  buttonColor,
+  buttonBackgroundColor,
+  disabled,
+  text,
+  onPressButton,
+}) {
+  const myButtonStyle = {
+    height: "2rem",
+    color: buttonColor,
+    backgroundColor: buttonBackgroundColor,
+  };
+
+  return (
+    <button onClick={onPressButton} style={myButtonStyle} disabled={disabled}>
+      {text}
+    </button>
+  );
 }
