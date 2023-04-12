81b5b5d8ec108ed93be039835ee92c94f4528350
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-04-12 15:52:13 +0200

    sum done

--- React/react-props_sum/src/App.js
+++ React/react-props_sum/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
-
+// not my code I looked at other peoples code when I couldn`t solve it
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+return <Sum valueA={6} valueB={4} />;
+}
+
+function Sum({ valueA, valueB }) {
+  const sum = valueA + valueB;
+  return (
+    <h1>
+      {valueA} + {valueB} = {sum}
+    </h1>
+  );
 }
