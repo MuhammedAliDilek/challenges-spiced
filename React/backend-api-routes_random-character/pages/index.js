6915bd0dd04a9c8603e5250f3975aa7d50c1800f
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-05-04 16:24:38 +0200

    random character done

--- React/backend-api-routes_random-character/pages/index.js
+++ React/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,20 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>Failed to load character</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Hello from Next.js!</h1>
+      <h2>{data.name}</h2>
+      <p>{data.description}</p>
+      <img src={data.image} alt={data.name} />{" "}
+      {/* I have no image data but when I do I can use this code */}
+    </div>
+  );
 }
