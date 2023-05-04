6cbbefb82c5ccdbb264e3e7a4da73c8907f1bf39
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-05-04 12:01:41 +0200

    change done

--- React/backend-basics_chance/server.js
+++ React/backend-basics_chance/server.js
@@ -1,3 +1,16 @@
 import { createServer } from "node:http";
-
+import Chance from "chance";
 // export const server = …
+const chance = new Chance();
+export const server = createServer((req, res) => {
+  res.statusCode = 200;
+  const name = chance.name();
+  const age = chance.age();
+  const gender = chance.gender();
+  const address = chance.address();
+  const city = chance.city();
+  const profession = chance.profession();
+  res.end(
+    `Hello my name is ${name} and I am ${age} years old ${gender}. I am a ${profession} working in ${city}. If you want to hunt me down I am waiting ${address}.`
+  );
+});
